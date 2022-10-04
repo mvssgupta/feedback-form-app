8858fa4b8849201bb200fd5369bd0c38a63b3025
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2022-10-04 16:19:41 +0530

    Added animation to the feedback list

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {motion , AnimatePresence} from "framer-motion"
 import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 
@@ -7,18 +8,43 @@ function feedbackList({feedback , handleDelete}) {
         return <p>No feedBack</p>
     }
 
-  return (
-  <div className="feedback-list">
-    {
-        feedback.map((item) => (
-        <FeedbackItem
-         key={item.id} 
-         item={item} 
-         handleDelete={handleDelete}
-         ></FeedbackItem>
-    ))}
-  </div>
-  )
+//by using motion and animate , here we created a faded effect while deletion 
+//and while creating a new feedback.
+    return (
+        <div className="feedback-list">
+        <AnimatePresence>
+          {
+              feedback.map((item) => (
+              <motion.div 
+                key={item.id}
+                initial = {{opacity:0}}
+                animate = {{opacity:1}}
+                exit = {{opacity:0}}
+              >
+                <FeedbackItem
+                key={item.id} 
+                item={item} 
+                handleDelete={handleDelete}
+                ></FeedbackItem>
+              </motion.div>
+          ))}
+        </AnimatePresence>
+        </div>
+        )
+
+//this code is with out animation.
+//   return (
+//   <div className="feedback-list">
+//     {
+//         feedback.map((item) => (
+//         <FeedbackItem
+//          key={item.id} 
+//          item={item} 
+//          handleDelete={handleDelete}
+//          ></FeedbackItem>
+//     ))}
+//   </div>
+//   )
 }
 
 feedbackList.propTypes = {
